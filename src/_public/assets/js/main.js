3fe762d2a1aef975ccb98bcb919283b4aae1a713
Dan Nguyen <ng_ngoc_tuong_dan@allgrow-labo.jp>
2022-09-28 08:50:00 +0700

    tuongdan

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -2,23 +2,29 @@ $(document).ready(function(){
   
   // auto slide
   $('.c-slider__list').slick({
-    autoplay: true,
     fade: true,
-    speed: 5000,
+      autoplay: true,
+      autoplayspeed: 5000,
+      speed: 2000,
+      arrows: false,
+      dots: false,
+      pauseOnFocus: false,
+      pauseOnHover: false,
   });
 
   //Check to see if the window is top if not then display button
+  $(".c-backtotop").addClass("c-hidetop");
   $(window).scroll(function(){
-      if ($(this).scrollTop() > 2500) {
-          $('.c-backtotop').fadeIn(1000);
+      if ($(this).scrollTop() > 80) {
+          $('.c-backtotop').removeClass("c-hidetop");
       } else {
-          $('.c-backtotop').fadeOut(1000);
+          $('.c-backtotop').addClass("c-hidetop");
       }
   });
 
   //Click event to scroll to top
   $('.c-backtotop').click(function(){
-      $('html, body').animate({scrollTop : 0},1200);
+      $('html, body').animate({scrollTop : 0},800);
       return false;
   });
 
@@ -27,6 +33,7 @@ $(document).ready(function(){
     $('.c-popup').toggleClass('is-popupopen');
     $('.c-popup__box').toggleClass('is-open');
     $('.c-btnmenu').toggleClass('is-open');
+    $('body').toggleClass('is-bdfixed');
     var src = $('.c-gnav__boxmenu img').attr('src');
     if( src == './assets/img/menu.png' ){ src ='./assets/img/cancel.png'}
     else{ src ='./assets/img/menu.png'}
@@ -36,7 +43,9 @@ $(document).ready(function(){
   $('.c-popup__overlay').click(function() {
     $('.c-popup').removeClass('is-popupopen');
     $('.c-popup__box').removeClass('is-open');
+    $('.c-btnmenu').removeClass('is-open');
     $('.c-gnav__boxmenu img').attr('src','./assets/img/menu.png')
+    $('body').removeClass('is-bdfixed');
  });
 
 });
